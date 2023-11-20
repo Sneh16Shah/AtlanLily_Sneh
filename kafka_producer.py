008eb671b61ff62b078bfec7eab61668367d5442
Sneh Shah <75805672+Sneh16Shah@users.noreply.github.com>
2023-11-20 17:34:36 +0530

    Update

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,21 +1,26 @@
 import random
 import time
+import logging
 from confluent_kafka import Producer
 
+# Configure logging to write to the file immediately without buffering
+logging.basicConfig(filename='producer_logs.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 kafka_config = {
-    'bootstrap.servers': 'localhost:9092',  
+    'bootstrap.servers': 'localhost:9092',
     'client.id': 'metadata-producer',
-    'acks': 1,  
-    'linger.ms': 0,  
+    'acks': 1,
+    'linger.ms': 0,
 }
 
 producer = Producer(kafka_config)
 
 topic = 'metadata-changes'
 
+
 def generate_random_metadata_event(id):
-    entity_id = str(id)#str(random.randint(1, 1000))
+    entity_id = str(id)
     event_type = random.choice(['update', 'create', 'delete'])
     metadata_info = f'Random metadata event for entity {entity_id}'
     return {
@@ -24,11 +29,21 @@ def generate_random_metadata_event(id):
         'metadata_info': metadata_info,
     }
 
-for _ in range(100000):
-    metadata_event = generate_random_metadata_event(_)
+
+for i in range(100000):
+    metadata_event = generate_random_metadata_event(i)
     producer.produce(topic, key='metadata_key', value=str(metadata_event))
     producer.flush()
-    print("Generated entry {}".format(_))
-    time.sleep(1) 
 
+    # Open the file each time to write immediately without buffering
+    with open('producer_logs.log', 'a') as log_file:
+        log_file.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - INFO - Generated entry {i} - Metadata Event: {metadata_event}\n")
+    
+    print("Generated entry", i)
+    time.sleep(1)
+
+# Flushing any remaining messages
+producer.flush()
+
+logging.info("Produced 50 random metadata events.")
 print("Produced 50 random metadata events.")
